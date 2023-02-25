a6f9004e254a900dd890f2537cc4d7789be44e25
Roberdan <Roberdan@fightthestroke.org>
2023-02-25 16:35:06 +0100

    release 1.0

--- Tests/PermissionsManagerTests/PermissionsManagerTests.swift
+++ Tests/PermissionsManagerTests/PermissionsManagerTests.swift
@@ -6,6 +6,6 @@ final class PermissionsManagerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertNotNil(PermissionsManager(permissions: []))
+        XCTAssertNotNil(PermissionsManager())
     }
 }
